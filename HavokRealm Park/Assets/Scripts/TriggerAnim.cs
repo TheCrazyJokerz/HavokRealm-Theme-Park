975f4ac82158755ca3be5a2d1fc9a5e041fd4b47
Luis Arias <luis.arias@enti.cat>
2024-05-18 21:04:56 +0200

    15

---
+++ HavokRealm Park/Assets/Scripts/TriggerAnim.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerAnim : MonoBehaviour
+{
+    [SerializeField] private Animator myAnim;
+
+    [SerializeField] private string animName;
+
+    [SerializeField] private GameObject triggerToHide;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myAnim.Play(animName, 0, 0.0f); //temps que triga en play anim
+            triggerToHide.SetActive(false);
+        }
+    }
+
+
+
+}
+
+
+
